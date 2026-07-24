3b1398d6f3e733ae11a6577b43a7846822017ccc
amaliaP12 <amaliapetre04@gmail.com>
2026-07-24 14:42:45 +0300

    Added logging statements

--- spring-boot/demo/src/main/java/com/example/demo/controller/UserController.java
+++ spring-boot/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import com.example.demo.model.User;
 
 @RestController
 public class UserController {
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController() {
         System.out.println("UserController initialized");
@@ -23,5 +27,14 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
 
+        return "Howdy! Check out the Logs to see the output...";
+    }
 }
